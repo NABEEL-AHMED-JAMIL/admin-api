782225d1048f3ceaf8646f23e7691b1448b9b67b
NABEEL-AHMED-JAMIL <nabeel.amd93@gmail.com>
2020-08-14 20:36:27 +0500

    init websocket commit

--- src/main/java/com/barco/admin/AdminApiApplication.java
+++ src/main/java/com/barco/admin/AdminApiApplication.java
@@ -3,10 +3,15 @@ package com.barco.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
+
+@EnableWebSocket
+@EnableScheduling
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.barco.*" })
 public class AdminApiApplication {
